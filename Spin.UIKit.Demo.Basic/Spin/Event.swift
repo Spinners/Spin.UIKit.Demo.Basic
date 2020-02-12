8133394fefd1b81182b18c03a6b32634ce5e7946
Thibault Wittemberg <twittemberg@genetec.com>
2020-02-11 21:16:05 -0500

    feedback: add Event

---
+++ Spin.UIKit.Demo.Basic/Spin/Event.swift
@@ -0,0 +1,15 @@
+//
+//  Event.swift
+//  Spin.UIKit.Demo.Basic
+//
+//  Created by Thibault Wittemberg on 2020-02-11.
+//  Copyright © 2020 Spinners. All rights reserved.
+//
+
+enum Event {
+    case start
+    case pause
+    case reset
+    case increase
+    case decrease
+}
