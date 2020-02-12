2b6a36cb358b33bf80da86dcbcc18b663917af57
Thibault Wittemberg <twittemberg@genetec.com>
2020-02-11 21:05:41 -0500

    project: add empty scenes

---
+++ Spin.UIKit.Demo.Basic/CombineViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CombineViewController.swift
+//  Spin.UIKit.Demo.Basic
+//
+//  Created by Thibault Wittemberg on 2020-02-11.
+//  Copyright © 2020 Spinners. All rights reserved.
+//
+
+import UIKit
+
+class CombineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
