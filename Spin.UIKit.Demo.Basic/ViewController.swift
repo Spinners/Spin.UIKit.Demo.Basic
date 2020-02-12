9451a423a02f902b87c8db37d7d7cba7d1048acd
Thibault Wittemberg <twittemberg@genetec.com>
2020-02-11 20:31:58 -0500

    Initial Commit

---
+++ Spin.UIKit.Demo.Basic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spin.UIKit.Demo.Basic
+//
+//  Created by Thibault Wittemberg on 2020-02-11.
+//  Copyright © 2020 Spinners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
