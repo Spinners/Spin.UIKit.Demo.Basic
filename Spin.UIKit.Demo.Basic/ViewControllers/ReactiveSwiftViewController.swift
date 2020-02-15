130cc79ba58c115d429b2ed56cf378c6dfbf3ecc
Thibault Wittemberg <twittemberg@genetec.com>
2020-02-15 17:43:42 -0500

    ui: improve buttons

--- Spin.UIKit.Demo.Basic/ViewControllers/ReactiveSwiftViewController.swift
+++ Spin.UIKit.Demo.Basic/ViewControllers/ReactiveSwiftViewController.swift
@@ -21,8 +21,15 @@ class ReactiveSwiftViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.resetButton.layer.cornerRadius = 20
+        self.resetButton.backgroundColor = .gray
+        self.resetButton.setTitleColor(.white, for: .normal)
 
-       let countdownSpin = Spinner
+        self.toggleButton.layer.cornerRadius = 20
+        self.toggleButton.backgroundColor = .gray
+        self.toggleButton.setTitleColor(.white, for: .normal)
+
+        let countdownSpin = Spinner
             .from(initialState: State.fixed(value: 10))
             .add(feedback: ReactiveFeedback(effect: decreaseEffect))
             .add(feedback: ReactiveFeedback(effect: increaseEffect))
@@ -53,19 +60,22 @@ private extension ReactiveSwiftViewController {
             self.counterLabel.textColor = .green
             self.toggleButton.isEnabled = true
             self.resetButton.isEnabled = false
+            self.resetButton.alpha = 0.5
             self.toggleButton.setTitle("Start", for: .normal)
         case .decreasing(let value, let paused):
             self.counterLabel.text = "\(value)"
             self.counterLabel.textColor = .red
             self.toggleButton.isEnabled = true
-            self.toggleButton.setTitle(paused ? "Start": "Pause", for: .normal)
+            self.toggleButton.setTitle(paused ? "Start": "Stop", for: .normal)
             self.resetButton.isEnabled = true
+            self.resetButton.alpha = 1.0
         case .increasing(let value, let paused):
             self.counterLabel.text = "\(value)"
             self.counterLabel.textColor = .blue
             self.toggleButton.isEnabled = true
-            self.toggleButton.setTitle(paused ? "Start": "Pause", for: .normal)
+            self.toggleButton.setTitle(paused ? "Start": "Stop", for: .normal)
             self.resetButton.isEnabled = true
+            self.resetButton.alpha = 1.0
         }
     }
 }
